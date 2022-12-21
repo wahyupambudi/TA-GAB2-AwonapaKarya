6cd0633281443f99ceaaa31c4c9d3c7039b60ab0
wahyupambudi <wahyupambudi823@gmail.com>
2022-12-21 15:12:19 +0700

    master: menambah fungsi upload gambar

--- Server/application/models/Mbarang.php
+++ Server/application/models/Mbarang.php
@@ -33,6 +33,50 @@ class Mbarang extends CI_Model
     // membuat model untuk post data barang
     function save_data($kd_brg, $nm_brg, $spek_brg, $jml_brg, $kondisi_brg, $tgl_buy_brg, $harga_brg, $img_brg, $token)
     {
+
+        $data = json_decode(file_get_contents("php://input"), true); // collect input parameters and convert into readable format
+
+        $fileName  =  $_FILES['imgbrg']['name'];
+        $tempPath  =  $_FILES['imgbrg']['tmp_name'];
+        $fileSize  =  $_FILES['imgbrg']['size'];
+
+        if (empty($fileName)) {
+            $errorMSG = json_encode(array("message" => "please select image", "status" => false));
+            echo $errorMSG;
+        } else {
+            // set upload folder path 
+            $upload_path = 'public/upload/';
+
+            // newfilename
+            $newfilename = uniqid() . $fileName;
+
+            // get image extension
+            $fileExt = strtolower(pathinfo($fileName, PATHINFO_EXTENSION));
+
+            // valid image extensions
+            $valid_extensions = array('jpeg', 'jpg', 'png', 'gif');
+
+            // allow valid image file formats
+            if (in_array($fileExt, $valid_extensions)) {
+                //check file not exist our upload folder path
+                if (!file_exists($upload_path . $fileName)) {
+                    // check file size '5MB'
+                    if ($fileSize < 5000000) {
+                        move_uploaded_file($tempPath, $upload_path . $newfilename); // move file from system temporary path to our upload folder path 
+                    } else {
+                        $errorMSG = json_encode(array("message" => "Sorry, your file is too large, please upload 5 MB size", "status" => false));
+                        echo $errorMSG;
+                    }
+                } else {
+                    $errorMSG = json_encode(array("message" => "Sorry, file already exists check upload folder", "status" => false));
+                    echo $errorMSG;
+                }
+            } else {
+                $errorMSG = json_encode(array("message" => "Sorry, only JPG, JPEG, PNG & GIF files are allowed", "status" => false));
+                echo $errorMSG;
+            }
+        }
+
         // check kd_brg apakah ada di table
         $this->db->select("kd_brg");
         $this->db->from("tb_barang");
@@ -52,7 +96,7 @@ class Mbarang extends CI_Model
                 "kondisi_brg" => $kondisi_brg,
                 "tgl_buy_brg" => $tgl_buy_brg,
                 "harga_brg" => $harga_brg,
-                "img_brg" => $img_brg
+                "img_brg" => $newfilename
             );
 
             // proses simpan data
